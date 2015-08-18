da94c6898bd8db6823d7d5258eff13e890d3177c
Denis Ryzhkov <denisr@denisr.com>
2015-08-18 11:25:38 +0300

    v0.2.0: Syncs clipboards of Midnight Commander and X Window System.

---
+++ README.md
@@ -0,0 +1,21 @@
+mc_xclip
+========
+
+Syncs clipboards of Midnight Commander and X Window System.
+
+Install:
+
+    sudo apt-get install xclip
+    sudo pip install mc_xclip
+
+    # If you use Display Manager:
+    echo 'mc_xclip &' >> .xprofile
+
+    # Else:
+    echo 'mc_xclip &' >> .xinitrc
+
+    # Reboot.
+
+mc_xclip version 0.2.0  
+Copyright (C) 2011-2015 by Denis Ryzhkov <denisr@denisr.com>  
+MIT License, see http://opensource.org/licenses/MIT
---
+++ setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+setup(
+    name='mc_xclip',
+    version='0.2.0',
+    description='Syncs clipboards of Midnight Commander and X Window System.',
+    long_description='''
+Install::
+
+    sudo apt-get install xclip
+    sudo pip install mc_xclip
+
+    # If you use Display Manager:
+    echo 'mc_xclip &' >> .xprofile
+
+    # Else:
+    echo 'mc_xclip &' >> .xinitrc
+
+    # Reboot.
+
+''',
+    url='https://github.com/denis-ryzhkov/mc_xclip',
+    author='Denis Ryzhkov',
+    author_email='denisr@denisr.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ],
+    scripts=[
+        'scripts/mc_xclip',
+    ],
+)
